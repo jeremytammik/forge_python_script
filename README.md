636aa9ee4ca74fd29c23aaec9ea2fd22d0bce929
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2016-12-13 17:17:56 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
-# forge_python_script
-Forge Python Scripts
+# Forge Python Scripts
+
+A collection of Forge Python scripts, currently with a count of one.
+
+
+## py_forge_formats.py
+
+Implements a Python wrapper around two basic Forge web service calls:
+
+- Authenticate an app.
+- Query the file formats currently supported by the translation processes.
+
+Here is the result of running this script at the time of writing:
+
+```
+$ ./py_forge_formats.py
+9 Forge output formats:
+  dwg: f2d, f3d, rvt
+  fbx: f3d
+  ifc: rvt
+  iges: f3d, fbx, iam, ipt, wire
+  obj: asm, f3d, fbx, iam, ipt, neu, prt, sldasm, sldprt, step, stp, stpz,
+    wire, x_b, x_t, asm.NNN, neu.NNN, prt.NNN
+  step: f3d, fbx, iam, ipt, wire
+  stl: f3d, fbx, iam, ipt, wire
+  svf: 3dm, 3ds, asm, catpart, catproduct, cgr, collaboration, dae, dgn,
+    dlv3, dwf, dwfx, dwg, dwt, dxf, exp, f3d, fbx, g, gbxml, iam, idw,
+    ifc, ige, iges, igs, ipt, jt, max, model, neu, nwc, nwd, obj, pdf,
+    prt, rcp, rvt, sab, sat, session, skp, sldasm, sldprt, smb, smt,
+    ste, step, stl, stla, stlb, stp, stpz, wire, x_b, x_t, xas, xpr,
+    zip, asm.NNN, neu.NNN, prt.NNN
+  thumbnail: 3dm, 3ds, asm, catpart, catproduct, cgr, collaboration, dae, dgn,
+    dlv3, dwf, dwfx, dwg, dwt, dxf, exp, f3d, fbx, g, gbxml, iam, idw,
+    ifc, ige, iges, igs, ipt, jt, max, model, neu, nwc, nwd, obj, pdf,
+    prt, rcp, rvt, sab, sat, session, skp, sldasm, sldprt, smb, smt,
+    ste, step, stl, stla, stlb, stp, stpz, wire, x_b, x_t, xas, xpr,
+    zip, asm.NNN, neu.NNN, prt.NNN
+```
+
+This script replaces and improves on the previous `forgeauth` and `forgeformats` Unix shell cURL scripts documented in the discussion of
+the [`cURL` wrapper scripts to list Forge file formats](http://thebuildingcoder.typepad.com/blog/2016/10/forge-intro-formats-webinars-and-fusion-360-client-api.html#3).
+
+For the sake of completeness, those two scripts have been added to this repository as well.
+
+
+## Setup and Usage
+
+Before you can make any use of the Forge web services, you will need to register an app and request the API client id and client secret for it
+at [developer.autodesk.com](https://developer.autodesk.com)
+&gt; [my apps](https://developer.autodesk.com/myapps).
+
+These scripts assume that you have stored these creadentials in the environment variables `FORGE_CLIENT_ID` and `FORGE_CLIENT_SECRET`.
+
+
+## <a name="98"></a>Author
+
+Jeremy Tammik,
+[The Building Coder](http://thebuildingcoder.typepad.com) and
+[The 3D Web Coder](http://the3dwebcoder.typepad.com),
+[ADN](http://www.autodesk.com/adn)
+[Open](http://www.autodesk.com/adnopen),
+[Forge Partner Development](http://forge.autodesk.com),
+[Autodesk Inc.](http://www.autodesk.com)
+
+
+## <a name="99"></a>License
+
+This sample is licensed under the terms of the [MIT License](http://opensource.org/licenses/MIT).
+Please see the [LICENSE](LICENSE) file for full details.
+
---
+++ py_forge_formats.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+#
+# /j/sh/py_forge.py
+#
+# Implement a Python wrapper around the Forge calls to
+# authenticate an app and query the file formats currently
+# supported by the translation processes.
+#
+# This script replaces and improves on the previous
+# forgeauth and forgeformats Unix shell cURL scripts
+# documented in
+#
+# http://thebuildingcoder.typepad.com/blog/2016/10/forge-intro-formats-webinars-and-fusion-360-client-api.html#3
+#
+# Copyright (C) 2016 Jeremy Tammik
+#
+import os, requests, textwrap
+
+verbose = False
+url_authenticate = 'https://developer.api.autodesk.com/authentication/v1/authenticate'
+url_formats = 'https://developer.api.autodesk.com/modelderivative/v2/designdata/formats'
+
+def forge_authenticate_app( client_id, client_secret, verbose ):
+  """Retrieve and return an authentication token
+  for this app's API key and secret."""
+
+  data = {
+    'client_id' : client_id,
+    'client_secret' : client_secret,
+    'grant_type' : 'client_credentials',
+    'scope' : 'data:read'
+  }
+  
+  r = requests.post(url_authenticate, data=data)
+
+  if verbose:
+    print '\nForge authenticate call:'
+    print '  Status:', r.status_code
+    print '  Headers:', r.headers['content-type']
+    print '  Content:', r.content
+
+  if 200 == r.status_code:
+    token = r.json()['access_token']
+  else:
+    token = None
+
+  return token
+  
+def forge_formats( token, verbose ):
+  """Retrieve and return the file formats currently
+   supported by the translation processes."""
+
+  headers = {'Authorization': 'Bearer ' + token}
+  r = requests.get(url_formats, headers=headers)
+
+  if verbose:
+    print '\nForge formats call:'
+    print '  Status:', r.status_code
+    print '  Headers:', r.headers['content-type']
+    print '  Content:', r.content
+
+  if 200 == r.status_code:
+    formats = r.json()['formats']
+  else:
+    formats = None
+
+  return formats
+
+def jprettyprint(formats):
+  "Prettyprint the JSON file format list returned by Forge."
+  keys = formats.keys()
+  keys.sort()
+  return '\n'.join( ['  ' + key + ': '
+    + textwrap.fill( ', '.join( formats[key] ), subsequent_indent='    ')
+      for key in keys] )
+
+def main():
+  """Authenticate ourselves with Forge and
+  list the file formats currently supported."""
+  
+  # My app credentials.
+  
+  client_id = os.environ['FORGE_CLIENT_ID']
+  client_secret = os.environ['FORGE_CLIENT_SECRET']
+
+  # Authenticate app and retrieve access token.
+  
+  token = forge_authenticate_app(
+    client_id, client_secret, verbose )
+  
+  if verbose: print 'token:', token
+
+  # Retrieve list of currently supported
+  # translation file formats.
+  
+  formats = forge_formats( token, verbose )
+  
+  # Present the results in a human readable manner.
+  
+  # s = json.dumps(formats, indent=2, sort_keys=True)
+  
+  s = jprettyprint(formats)
+  s = s.replace('\.\d+$', '.NNN')
+    
+  print len(formats), 'Forge output formats:'
+  print s
+  
+if __name__ == "__main__":
+  main()
